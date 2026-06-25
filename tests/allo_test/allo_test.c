cca263583d9a5efbb959bbff750e8478796e6fb9
Marc Fong <marcjohnfong@gmail.com>
2026-06-25 20:37:41 +0800

    Add assert functions to allo_test

--- tests/allo_test/allo_test.c
+++ tests/allo_test/allo_test.c
@@ -1,5 +1,4 @@
 #include "allo_test.h"
-#include "allo/allo_status.h"
 #include "allo/internal/allo_math.h"
 #include <stdarg.h>
 #include <stddef.h>
@@ -30,19 +29,3 @@ void *test_utils_malloc_aligned(void **dest, size_t size, size_t align,
   uintptr_t aligned_addr = allo_math_align_up(addr, align);
   return (void *)aligned_addr;
 }
-
-void allo_test_assert_status(allo_status expected, allo_status actual,
-                             const char *message, size_t line) {
-  enum { BUFSIZE = 1 << 6 };
-  if (expected != actual) {
-    char buffer[BUFSIZE] = {0};
-    size_t offset = 0;
-    allo_test_snprintf(buffer, BUFSIZE, &offset, "%s: ", message);
-    allo_test_snprintf(
-        buffer, BUFSIZE, &offset, "expected=%d (%s) actual=%d (%s)", expected,
-        allo_status_str(expected), actual, allo_status_str(actual));
-    UnityFail(buffer, line);
-  }
-}
-
-
--- tests/allo_test/allo_test_assert.c
+++ tests/allo_test/allo_test_assert.c
@@ -1,1 +1,43 @@
 #include "allo_test_assert.h"
+#include "allo_test_io.h"
+#include <unity_internals.h>
+
+void allo_test_assert_status(allo_status expected, allo_status actual,
+                             const char *message, size_t line) {
+  enum { BUFSIZE = 1 << 6 };
+  if (expected != actual) {
+    char buf[BUFSIZE] = {0};
+    size_t offset = 0;
+    allo_test_snprintf(buf, BUFSIZE, &offset, "%s: ", message);
+    allo_test_snprintf(buf, BUFSIZE, &offset, "expected=%d (%s) actual=%d (%s)",
+                       expected, allo_status_str(expected), actual,
+                       allo_status_str(actual));
+    UnityFail(buf, line);
+  }
+}
+
+void allo_test_assert_mem(uintptr_t expected, uintptr_t actual,
+                          const char *message, size_t line) {
+  enum { BUFSIZE = 1 << 6 };
+  if (expected != actual) {
+    char buf[BUFSIZE] = {0};
+    size_t offset = 0;
+    allo_test_snprintf(buf, BUFSIZE, &offset, "%s: ", message);
+    allo_test_snprintf(buf, BUFSIZE, &offset, "expected=0x%lx actual=0x%lx",
+                       expected, actual);
+    UnityFail(buf, line);
+  }
+}
+
+void allo_test_assert_size_t(size_t expected, size_t actual,
+                             const char *message, size_t line) {
+  enum { BUFSIZE = 1 << 6 };
+  if (expected != actual) {
+    char buf[BUFSIZE] = {0};
+    size_t offset = 0;
+    allo_test_snprintf(buf, BUFSIZE, &offset, "%s: ", message);
+    allo_test_snprintf(buf, BUFSIZE, &offset, "expected=%zu actual=%zu",
+                       expected, actual);
+    UnityFail(buf, line);
+  }
+}
