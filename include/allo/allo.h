497af59f9a1615f33d0c3ee250937615a821ca23
Marc Fong <marcjohnfong@gmail.com>
2026-07-22 22:48:54 +0800

    Update docs

--- include/allo/allo.h
+++ include/allo/allo.h
@@ -1,18 +1,6 @@
 #ifndef ALLO_H
 #define ALLO_H
 
-// DOCUMENTATION
-//
-// This library implements different types of memory allocators, along with a
-// generic allocator interface so that users can choose what allocator strategy
-// to use via dynamic dispatch.
-//
-// Allocator implementations:
-// - Allocator interface         (allo/allocator.h)
-// - Fixed sized bump allocator  (allo/bump.h)
-// - Fixed sized stack allocator (allo/stack.h)
-// - Fixed sized pool allocator  (allo/pool.h)
-
 // IWYU pragma: begin_exports
 #include "bump.h"
 #include "config.h"
